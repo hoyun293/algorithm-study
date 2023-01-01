9537419cca69ab23a066588c6310d926abb68fde
hoyeon <hoyun293@naver.com>
2023-01-01 11:39:19 +0900

    programmers - 연속 부분 수열 합의 개수

---
+++ programmers/연속 부분 수열 합의 개수.js
@@ -0,0 +1,38 @@
+function solution(elements) {
+    let answer = 0;
+    let dp = new Array(1001).fill(0).map(_=>new Array(1001).fill(0));
+    let elementSet = new Set();
+
+    // 길이가 1인 경우 각각 처리
+    for(let i = 0 ; i < elements.length; i++){
+        elementSet.add(elements[i]);
+    }
+
+    // 길이가 2인 연속 부분수열을 먼저 구한다.
+    for(let i = 0; i < elements.length - 2; i++){
+        dp[i][2] = elements[i] + elements[i+1];
+        elementSet.add(dp[i][2]);
+    }
+    // 마지막 경우 첫 번째 원소를 읽는다.
+    dp[elements.length-1][2] = elements[elements.length-1] + elements[0];
+    elementSet.add(dp[elements.length-1][2]);
+
+    // 길이가 2인 연속 부분수열 값을 이용해 그 이후의 연속 부분수열 값을 구한다.
+    for(let i = 0 ; i < elements.length - 1 ; i++){
+        for(let j = 3; j <= elements.length; j++){
+            dp[i][j] = dp[i][j-1] = elements[(i+j-1)%elements.length];
+            elementSet.add(dp[i][j]);
+        }
+    }
+    // 길이가 n인 경우 모든 수를 합한다.
+    sum = 0;
+    for(let i = 0 ; i < elements.length; i++){
+        sum += elements[i];
+    }
+    elementSet.add(sum);
+    answer = elementSet.size;
+    for(e in elementSet){
+        console.log(e);
+    }
+    return answer;
+}
